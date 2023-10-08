bb0fb1e6fe49efb4b9258ce87c6af1d4d57def74
taanngent <72206563+taanngent@users.noreply.github.com>
2023-10-08 19:13:47 +0530

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+      home: Scaffold(
+    backgroundColor: Color.fromARGB(255, 16, 181, 213),
+    body: Center(child: Text("MY FIRST APP")),
+  )));
+}
